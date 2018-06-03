add5f648dd7d6f3e4ce4c67d42e702c94ab1cf4e
Muhammad Affan Qureshi <affan.qureshi114@gmail.com>
2018-06-03 17:39:58 +0100

    Removed getCycleIdx function

--- graphGen/graphGen.py
+++ graphGen/graphGen.py
@@ -1,30 +1,26 @@
 import numpy as np
 import random
 
-def getCycleIndex(noOfNodes):
-
-    tmp = np.array([ 2**(-i) for i in range(1,noOfNodes)])
-    probs = tmp / np.sum(tmp)
-    cumProbs = np.cumsum(probs)
-
-    return np.digitize(random.random(), cumProbs)
-
-def genRandomGraphs(noOfNodes, noOfGraphs, graphSparcity):
+def GenRandomGraphs(noOfNodes, noOfGraphs, graphDensity):
 
     graphs = np.zeros((noOfGraphs, noOfNodes, noOfNodes), dtype=np.int8)
 
     for i in range(noOfNodes-1):
         graphs[:, i, i+1] = 1
     
+    tmp = np.array([ 2**(-i) for i in range(1,noOfNodes)])
+    probs = tmp / np.sum(tmp)
+    cumProbs = np.cumsum(probs)
+
     for n in range(noOfGraphs):
 
-        idx = getCycleIndex(noOfNodes)
+        idx = np.digitize(random.random(), cumProbs)
         for i in range(0,idx+1):
             graphs[n, noOfNodes-i-1, idx-i] = 1
 
         emptyEdges = noOfNodes**2 - noOfNodes - idx + 1
         extraEdges = int(
-            random.triangular(0, emptyEdges, emptyEdges*graphSparcity)) 
+            random.triangular(0, emptyEdges, emptyEdges*graphDensity)) 
 
         for i in range(extraEdges):
 
@@ -42,7 +38,7 @@ def genRandomGraphs(noOfNodes, noOfGraphs, graphSparcity):
     return graphs
 
 def main():
-    g = genRandomGraphs(6,2,0.01)
+    g = GenRandomGraphs(6,1,0.01)
     print(g)
 
 if __name__ == "__main__":
