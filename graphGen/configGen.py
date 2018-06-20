38a083b8372381dd5ce4c871c7b674a650ab0baa
Muhammad Affan Qureshi <affan.qureshi114@gmail.com>
2018-06-20 23:25:16 +0100

    Added documentation to the scripts

--- graphGen/configGen.py
+++ graphGen/configGen.py
@@ -1,60 +1,83 @@
 import numpy as np
-import graphGen 
+import graphGen
+import argparse
 
 def InNeighbours(graph):
+    """
+    serialize the graph as a string object and return the in neighbours for the graph
+    :param graph: an array indicating the graph object
+    :return: string object representation for the graph
+    """
     string = '<< '
-    for i in range(np.size(graph,1)):
+    for i in range(np.size(graph, 1)):
         s = '{'
-        for j in range(np.size(graph,0)):
+        for j in range(np.size(graph, 0)):
             if graph[j][i] == 1:
                 if s == '{':
                     s = s + str(j + 1)
                 else:
                     s = s + ', ' + str(j + 1)
-        
-        if i == np.size(graph,1) - 1:
+
+        if i == np.size(graph, 1) - 1:
             string = string + s + '} >>'
         else:
             string = string + s + '}, '
-    
     return string
 
+
 def OutNeighbours(graph):
+    """
+    serialize the graph as a string object and return the out neighbours for the graph
+    :param graph: this is an array indicating the graph object
+    :return: string representation for the graph
+    """
     string = '<< '
-    for i in range(np.size(graph,0)):
+    for i in range(np.size(graph, 0)):
         s = '{'
-        for j in range(np.size(graph,1)):
+        for j in range(np.size(graph, 1)):
             if graph[i][j] == 1:
                 if s == '{':
                     s = s + str(j + 1)
                 else:
                     s = s + ', ' + str(j + 1)
-        
-        if i == np.size(graph,1) - 1:
+
+        if i == np.size(graph, 1) - 1:
             string = string + s + '} >>'
         else:
             string = string + s + '}, '
 
     return string
 
-if __name__ == "__main__":
 
-    
-    nodes = 6
-    graphs = graphGen.GenRandomGraphs(nodes,2,0.01)
+def main():
+    """tool to determine the in neighbours and out neighbours for the graph"""
+    parser = argparse.ArgumentParser(
+        description='Script to generates strings use to define in and out \
+        neighbours in a configuration file')
+
+    parser.add_argument('--number_of_nodes', '-n', default=6,
+                        help='Specify the number of nodes for the graph',
+                        action='store')
+
+    parser.add_argument('--number_of_graphs', '-g', default=1,
+                        help='Specify the number of graphs',
+                        action='store')
+
+    parser.add_argument('--density', '-d', default=0.1,
+                        help='number of extra edges added randomly with continuous triangular distribution or with peak \
+                            at density, value should be between 0 and 1',
+                        action='store')
+
+    cli_args = parser.parse_args()
+    number_of_nodes = int(cli_args.number_of_nodes)
+    number_of_graphs = int(cli_args.number_of_graphs)
+    density = float(cli_args.density)
+    graphs = graphGen.GenRandomGraphs(number_of_nodes, number_of_graphs, density)
     inNeigh0 = InNeighbours(graphs[0])
     outNeigh0 = OutNeighbours(graphs[0])
-
-    # file = open(filename, 'w')
-    # for line in file:
-    #     if 'IN_NEIGHBOURS' in line:
-    #         line = 'IN_NEIGHBOURS == ' + inNeigh0
-    #     elif 'OUT_NEIGHBOURS' in line:
-    #         line = 'OUT_NEIGHBOURS == ' + outNeigh0
-    #     elif 'NODES' in line:
-    #         line = 'NODES == ' + nodes
-
     print(graphs[0])
     print(inNeigh0)
     print(outNeigh0)
 
+if __name__ == "__main__":
+    main()
--- graphGen/graphGen.py
+++ graphGen/graphGen.py
@@ -1,26 +1,35 @@
 import numpy as np
 import random
+import argparse
 
 def GenRandomGraphs(noOfNodes, noOfGraphs, graphDensity):
-
+    """
+    generates a random strongly connected digraph
+    :param noOfNodes: the number of nodes for the graph
+    :param noOfGraphs: the number of graphs
+    :param graphDensity: number of extra edges added randomly with continious \
+                        triangular distribution with peak \
+                        at density, value should be between 0 and 1
+    :return: array of randomly generated graphs
+    """
     graphs = np.zeros((noOfGraphs, noOfNodes, noOfNodes), dtype=np.int8)
 
     for i in range(noOfNodes-1):
         graphs[:, i, i+1] = 1
-    
-    tmp = np.array([ 2**(-i) for i in range(1,noOfNodes)])
+
+    tmp = np.array([2**-i for i in range(1, noOfNodes)])
     probs = tmp / np.sum(tmp)
     cumProbs = np.cumsum(probs)
 
     for n in range(noOfGraphs):
 
         idx = np.digitize(random.random(), cumProbs)
-        for i in range(0,idx+1):
+        for i in range(0, idx+1):
             graphs[n, noOfNodes-i-1, idx-i] = 1
 
         emptyEdges = noOfNodes**2 - noOfNodes - idx + 1
         extraEdges = int(
-            random.triangular(0, emptyEdges, emptyEdges*graphDensity)) 
+            random.triangular(0, emptyEdges, emptyEdges*graphDensity))
 
         for i in range(extraEdges):
 
@@ -37,10 +46,32 @@ def GenRandomGraphs(noOfNodes, noOfGraphs, graphDensity):
 
     return graphs
 
-def main():
-    g = GenRandomGraphs(6,1,0.01)
+
+def main(noOfNodes=6, noOfGraphs=1, density=0.1):
+    g = GenRandomGraphs(noOfNodes,noOfGraphs,density)
     print(g)
 
+
 if __name__ == "__main__":
-    main()
-    
+    parser = argparse.ArgumentParser(
+        description='Script to generate random graphs')
+
+    parser.add_argument('--number_of_nodes', '-n', default=6,
+                        help='Specify the number of nodes for the graph',
+                        action='store')
+
+    parser.add_argument('--number_of_graphs', '-g', default=1,
+                        help='Specify the number of graphs',
+                        action='store')
+
+    parser.add_argument('--density', '-d', default=0.1,
+                        help='number of extra edges added randomly with \
+                        continious triangular distribution or with peak \
+                        at density, value should be between 0 and 1',
+                        action='store')
+
+    cli_args = parser.parse_args()
+    number_of_nodes = int(cli_args.number_of_nodes)
+    number_of_graphs = int(cli_args.number_of_graphs)
+    density = float(cli_args.density)
+    main(number_of_nodes, number_of_graphs, density)
